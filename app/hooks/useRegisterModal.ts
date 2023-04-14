ec2d558b7dc54842c848ec526b268113214cc564
jin <jin>
2023-04-14 16:42:50 -0400

    04141642

--- app/hooks/useRegisterModal.ts
+++ app/hooks/useRegisterModal.ts
@@ -1,15 +1,15 @@
-import { create } from 'zustand'
+import { create } from "zustand"
 
 interface RegisterModalStore {
-    isOpen: boolean
-    onOpen: () => void
-    onClose:()=>void
+  isOpen: boolean
+  onOpen: () => void
+  onClose: () => void
 }
 
 const useRegisterModal = create<RegisterModalStore>((set) => ({
-    isOpen: true,
-    onOpen: () => set({ isOpen: true }),
-    onClose:()=>set({isOpen:false})
+  isOpen: true,
+  onOpen: () => set({ isOpen: true }),
+  onClose: () => set({ isOpen: false }),
 }))
 
 export default useRegisterModal
